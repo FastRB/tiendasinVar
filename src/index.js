812a3dc6c5abd93ac42c70adff55917ee9d4e3b6
FastRB <rbucio123@live.com.mx>
2023-03-12 15:52:53 -0600

    vari

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from "./app"; // Equivale al codigo que esta en app.js
 import './database';
+import { PORT } from "./config";
 
-app.listen(3000);
-console.log("Servidor en puerto", 3000);
+app.listen(PORT);
+console.log("Servidor en puerto", PORT);
